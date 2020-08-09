fdc958843d3f2eb860b6d8d0ca19cbc6806e042e
sujin <sujinee9797@gmail.com>
2020-08-10 01:26:09 +0900

    컨트롤러 생성

--- src/main/java/com/zzzng/webservice/Application.java
+++ src/main/java/com/zzzng/webservice/Application.java
@@ -1,13 +1,13 @@
-package com.zzzng.demo;
+package com.zzzng.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
